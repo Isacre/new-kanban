8c93e8f2156dfe51362b8112da5efec4a81dbea3
Isaac Melo <isaacalvesx7@gmail.com>
2022-05-21 10:41:12 -0300

    kanban alterations locally saved

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import Board from "./board";
 import HeaderComponent from "./header/";
+import { SaveOnLocal } from "../../utils";
+import { useDispatch } from "react-redux";
+import * as REDUX from "../../redux/kanban/index";
 
 const Component = styled.div`
   min-width: 100vw;
   height: 100vh;
-  background-color: #fcffe7;
+  background-color: rgb(255, 255, 255);
 `;
 
 export default function MainPage() {
+  const localData = localStorage.getItem("kanban");
+  const data = localData && JSON.parse(localData);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localData) {
+      SaveOnLocal("kanban", data);
+    }
+    dispatch(REDUX.updateKanban(data.columns));
+  }, [localData]);
   return (
     <Component>
       <HeaderComponent />
